acb860cc576e1b2b4e97b8f1201a3fcbfd5fd86c
fabiannabil1 <fabiannabil00@gmail.com>
2025-05-30 22:05:00 +0700

    up

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,8 @@
+class AppRoutes {
+  static const login = '/login';
+  static const home = '/home';
+  static const register = '/register';
+  static const landing = '/landing';
+  static const profile = '/profile';
+  // tambahkan route lain sesuai kebutuhan
+}
